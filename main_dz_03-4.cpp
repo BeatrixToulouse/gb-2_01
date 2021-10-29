c2c734f96a7bf97f30c65b22213b47092206f649
BeatrixToulouse <yulia.yushina_ext@leroymerlin.ru>
2021-10-29 22:47:16 +0300

    DZ-03 task 4 fix5

--- main_dz_03-4.cpp
+++ main_dz_03-4.cpp
@@ -1,55 +1,7 @@
 #include <iostream>
 
-enum class Suit { heart, diamond, club, spade};
-enum class Value : int { ace = 1, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, aceG};
-
-std::string suit_to_string(Suit s) {
-    switch (s) {
-    case Suit::heart:
-        return "HEART";
-    case Suit::diamond:
-        return "DIAMOND";
-    case Suit::club:
-        return "CLUB";
-    case Suit::spade:
-        return "SPADE";
-    default:
-        return "";
-    }
-}
-
-std::string value_to_string(Value v) {
-    switch (v) {
-        case Value::ace:
-            return "ACE";
-        case Value::two:
-            return "2";
-        case Value::three:
-            return "3";
-        case Value::four:
-            return "4";
-        case Value::five:
-            return "5";
-        case Value::six:
-            return "6";
-        case Value::seven:
-            return "7";
-        case Value::eight:
-            return "8";
-        case Value::nine:
-            return "9";
-        case Value::ten:
-            return "10";
-        case Value::jack:
-            return "JACK";
-        case Value::queen:
-            return "qUEEN";
-        case Value::king:
-            return "KING";
-        default:
-            return "ACE(11)";
-    }
-}
+enum Suit { heart, diamond, club, spade};
+enum Value { ace = 1, two, three, four, five, six, seven, eight, nine, ten, jack = 10, queen = 10, king = 10, aceG = 11};
 
 class Card {
 private:
@@ -70,24 +22,25 @@ public:
 void Card::flip() {
     is_back = !is_back;
 }
-std::string Card::get_back() const { 
-    if (is_back == 0) {
-        return "FALSE";
-    } else {
-        return "TRUE";
-    }
-};
 Value Card::get_value() { return value; };
 Suit Card::get_suit() { return suit; };
+std::ostream& operator<< (std::ostream& c, Value &v) {
+    c << v;
+    return c;
+};
+std::ostream& operator<< (std::ostream& c, Suit &s) {
+    c << s;
+    return c;
+}
 std::ostream &operator<< (std::ostream &c, Card &card) { 
-    c << value_to_string(card.get_value()) << " " << suit_to_string(card.get_suit()) << " " << card.get_back();
+    c << card.get_value() << " " << card.get_suit() << " " << card.is_back;
     return c;
 }
 
 int main() {
     using namespace std;
 
-    Card c(Suit::club, Value::king, 0);
+    Card c(Suit::club, Value::aceG, 0);
     cout << c << endl;
     c.flip();
     cout << c << endl;
