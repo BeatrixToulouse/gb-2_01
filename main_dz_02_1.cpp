64b17768e36b25a4ff7746d72ae460a95f8dc39d
BeatrixToulouse <yulia.yushina_ext@leroymerlin.ru>
2021-10-29 07:06:04 +0300

    DZ-02 fixed task 1

--- main_dz_02_1.cpp
+++ main_dz_02_1.cpp
@@ -11,51 +11,51 @@ public:
     Person(std::string name, int age, char gendre, int weight) :
         name(name), age(age), gendre(gendre), weight(weight) {};
     void set_name(std::string);
-    std::string get_name();
+    std::string get_name() const;
     void set_age(int);
-    int get_age();
+    int get_age() const;
     void set_gendre(char);
-    char get_gendre();
+    char get_gendre() const;
     void set_weight(int);
-    int get_weight();
+    int get_weight() const;
 };
 
 void Person::set_name(std::string name) { this->name = name; };
-std::string Person::get_name() { return name; };
+std::string Person::get_name() const { return name; };
 void Person::set_age(int age) { this->age = age; };
-int Person::get_age() { return age; };
+int Person::get_age() const { return age; };
 void Person::set_gendre(char gendre) { this->gendre = gendre; };
-char Person::get_gendre() { return gendre; };
+char Person::get_gendre() const { return gendre; };
 void Person::set_weight(int weight) { this->weight = weight; };
-int Person::get_weight() { return weight; };
+int Person::get_weight() const { return weight; };
 
 class Student : public Person {
 private:
     int year;
-    int counter = 0;
+    std::int16_t student_id;
+    static int counter;
 public:
     Student(std::string name, int age, char gendre, int weight, int year) :
         Person(name, age, gendre, weight) {
+            ++Student::counter;
+            this->student_id = Student::counter;
             this->year = year;
         };
     void set_year(int);
-    void print();
-    friend int get_counter(const Student& st) { return st.counter; };
-    friend void set_counter(Student&, int);
+    std::string print() const;
+    static int get_counter() { return Student::counter; };
 };
-
+int Student::counter = 0;
 void Student::set_year(int year) {
     this->year = year;
 };
-void Student::print() {
-    std::cout << "STUDENT " << this->counter << ": name -> " << get_name();
-    std::cout << " age -> " << get_age();
-    std::cout << " gendre -> " << get_gendre();
-    std::cout << " weight -> " << get_weight();
-    std::cout << " year - " << this->year << std::endl;
+std::string Student::print() const {
+    return "STUDENT " + std::to_string(student_id) + ": name -> " + get_name() +
+    " age -> " + std::to_string(get_age()) +
+    " gendre -> " + std::to_string(get_gendre()) +
+    " weight -> " + std::to_string(get_weight()) +
+    " year - " + std::to_string(this->year) + "\n";
 };
-void set_counter(Student& st, int number) { st.counter = number; };
-
 int main() {
     using namespace std;
     Student s1("Igor", 25, 'm', 82, 2018);
@@ -69,9 +69,11 @@ int main() {
     };
 
     for (int i = 0; i < 3; i++) {
-        set_counter(*(Student *)students[i], ++i);
-        static_cast <Student*> (students[--i])->print();
+        // static_cast <Student*> (students[i])->set_counter();
+        std::cout << static_cast <Student*> (students[i])->print();
     }
+
+    cout << "COUNTER: " << Student::get_counter() << endl;
 
     return 0;
 }
